6f33dc21b48cef77d049f5b4165df62790321286
Vincent Stollenwerk <vstollen@gmail.com>
2018-12-05 15:08:33 +0100

    Solve Day 2 Part Two (find fabric boxes)

--- src/checksum/Main.kt
+++ src/checksum/Main.kt
@@ -6,6 +6,7 @@ fun main(args : Array<String>) {
 
     val file = File("input/day2_checksum.txt")
 
+    // Generate Checksum
     var letterTwoTimes = 0
     var letterThreeTimes = 0
 
@@ -18,4 +19,27 @@ fun main(args : Array<String>) {
     }
 
     System.out.println(letterTwoTimes * letterThreeTimes)
+
+    // Find fabric boxes
+
+    file.forEachLine { boxId ->
+
+        file.forEachLine {
+            var difference = 0
+            val differentIndices = HashSet<Int>()
+
+            for (i in 0 until it.length) {
+                if (boxId[i] != it[i]) {
+                    difference++
+                    differentIndices.add(i)
+                }
+
+                if (difference > 1) break
+            }
+
+            if (difference == 1) {
+                System.out.println(boxId.removeRange(differentIndices.first(), differentIndices.first() + 1))
+            }
+        }
+    }
 }
