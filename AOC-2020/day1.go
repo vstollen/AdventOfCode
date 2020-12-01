3dd1126087920e149f4e5acdededcc8205ff2803
Vincent Stollenwerk <vstollen@gmail.com>
2020-12-01 10:27:28 +0100

    Implement 2020 - Day 1 Part 2

--- AOC-2020/day1.go
+++ AOC-2020/day1.go
@@ -11,6 +11,12 @@ import (
 func ExpenseReport() {
 	data := readData()
 
+	twoNumbers(data)
+	threeNumbers(data)
+}
+
+func twoNumbers(data []int) {
+	fmt.Println("Starting search for two numbers.")
 	for _, elem1 := range data {
 		for _, elem2 := range data {
 			if elem1 == elem2 {
@@ -24,6 +30,37 @@ func ExpenseReport() {
 				fmt.Printf("Solution: %v * %v = %v", elem1, elem2, solution)
 				return
 			}
+		}
+	}
+}
+
+func threeNumbers(data []int) {
+	fmt.Printf("Starting search for three numbers.")
+
+	for _, elem1 := range data {
+		for _, elem2 := range data {
+			if elem1 == elem2 {
+				continue
+			}
+
+			if elem1 + elem2 >= 2020 {
+				continue
+			}
+
+			for _, elem3 := range data {
+				if elem3 == elem1 || elem3 == elem2 {
+					continue
+				}
+
+				if elem1 + elem2 + elem3 == 2020 {
+					solution := elem1 * elem2 * elem3
+
+					fmt.Printf("%v + %v + %v = 2020\n", elem1, elem2, elem3)
+					fmt.Printf("Solution: %v * %v * %v = %v", elem1, elem2, elem3, solution)
+					return
+				}
+			}
+
 		}
 	}
 }
