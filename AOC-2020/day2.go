78e466ac9f59d5c633a5f0639eb977889c9dd62f
Vincent Stollenwerk <vstollen@gmail.com>
2020-12-02 10:53:31 +0100

    Refactor 2020 - Day 2 Part 1

--- AOC-2020/day2.go
+++ AOC-2020/day2.go
@@ -6,12 +6,19 @@ import (
 	"strings"
 )
 
+type password struct {
+	pos1, pos2 int
+	letter     rune
+	password   string
+}
+
 func CountValidPasswords() {
-	data := ReadData("day_2.txt")
+	stringData := ReadData("day_2.txt")
+	data := parsePasswords(stringData)
 
 	validPasswords := 0
 	for _, elem := range data {
-		if isValid(elem) {
+		if elem.oldIsValid() {
 			validPasswords++
 		}
 	}
@@ -19,30 +26,48 @@ func CountValidPasswords() {
 	fmt.Printf("There are %v valid passwords.", validPasswords)
 }
 
-func isValid(passwordWithPolicy string) bool {
-	parts := strings.Split(passwordWithPolicy, " ")
-
-	minmax := strings.Split(parts[0], "-")
-	min, _ := strconv.Atoi(minmax[0])
-	max, _ := strconv.Atoi(minmax[1])
-
-	letter := rune(parts[1][0])
-	password := parts[2]
+// Checker for the old password policy
+func (p password) oldIsValid() bool {
 
 	letterCount := 0
-	for _, passwordLetter := range password {
-		if passwordLetter == letter {
+	for _, letter := range p.password {
+		if letter == p.letter {
 			letterCount++
 		}
 	}
 
-	if letterCount < min {
+	if letterCount < p.pos1 {
 		return false
 	}
 
-	if letterCount > max {
+	if letterCount > p.pos2 {
 		return false
 	}
 
 	return true
+}
+
+func parsePasswords(data []string) []password {
+	var passwords = make([]password, len(data))
+
+	for i, _ := range data {
+
+		parts := strings.Split(data[i], " ")
+
+		numbers := strings.Split(parts[0], "-")
+		pos1, _ := strconv.Atoi(numbers[0])
+		pos2, _ := strconv.Atoi(numbers[1])
+
+		letter := rune(parts[1][0])
+		passwordText := parts[2]
+
+		passwords[i] = password{
+			pos1:     pos1,
+			pos2:     pos2,
+			letter:   letter,
+			password: passwordText,
+		}
+	}
+
+	return passwords
 }
