d1a3a9ccc498e733019264782a18414d432f085f
Vincent Stollenwerk <vstollen@gmail.com>
2020-12-05 21:32:47 +0100

    Implement 2020 - Day 5 Part 2

--- AOC-2020/day5.go
+++ AOC-2020/day5.go
@@ -7,31 +7,48 @@ import (
 func FindHighestSeatId() {
 	data := ReadData("day_5.txt")
 
+	occupiedSeats := map[int]bool{}
+
 	highestSeatId := -1
 	for _, boardingPass := range data {
-		rowLow := 0
-		rowHigh := 127
 
-		seatLow := 0
-		seatHigh := 7
+		seatId := calcSeatId(boardingPass)
 
-		for _, letter := range boardingPass {
-			switch letter {
-			case 'F':
-				rowHigh = (rowLow + rowHigh) / 2
-			case 'B':
-				rowLow = ((rowLow + rowHigh) / 2) + 1
-			case 'L':
-				seatHigh = (seatLow + seatHigh) / 2
-			case 'R':
-				seatLow = ((seatLow + seatHigh) / 2) + 1
-			}
-		}
-		if rowLow*8+seatLow > highestSeatId {
-			highestSeatId = rowLow*8 + seatLow
+		occupiedSeats[seatId] = true
+
+		if seatId > highestSeatId {
+			highestSeatId = seatId
 		}
 	}
 
 	fmt.Printf("Highest Seat ID: %v\n", highestSeatId)
 
+	for i := 0; i < highestSeatId; i++ {
+		if !occupiedSeats[i] {
+			fmt.Printf("Seat ID %v is free.\n", i)
+		}
+	}
+}
+
+func calcSeatId(boardingPass string) int {
+	rowLow := 0
+	rowHigh := 127
+
+	seatLow := 0
+	seatHigh := 7
+
+	for _, letter := range boardingPass {
+		switch letter {
+		case 'F':
+			rowHigh = (rowLow + rowHigh) / 2
+		case 'B':
+			rowLow = ((rowLow + rowHigh) / 2) + 1
+		case 'L':
+			seatHigh = (seatLow + seatHigh) / 2
+		case 'R':
+			seatLow = ((seatLow + seatHigh) / 2) + 1
+		}
+	}
+
+	return rowLow*8 + seatLow
 }
