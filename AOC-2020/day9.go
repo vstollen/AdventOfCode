900f7b1b679516531d1900511ed25264b31c13a9
Vincent Stollenwerk <vstollen@gmail.com>
2020-12-09 19:02:28 +0100

    Implement 2020 - Day 9 Part 2

--- AOC-2020/day9.go
+++ AOC-2020/day9.go
@@ -7,12 +7,45 @@ func XMASCypher() {
 
 	numbers := arrayAtoi(data)
 
+	invalidNumber := findInvalidNumber(numbers)
+	fmt.Printf("No sum for %v\n", invalidNumber)
+
+	setWithSum := findSetWithSum(invalidNumber, numbers)
+	fmt.Printf("The smallest and largest number adding up to %v summed together is: %v", invalidNumber, setWithSum[0]+setWithSum[len(setWithSum)-1])
+}
+
+func findInvalidNumber(numbers []int) int {
 	for i := 25; i < len(numbers); i++ {
 		if !sumExists(numbers[i], numbers[i-25:i]) {
-			fmt.Printf("No sum for %v\n", numbers[i])
-			break
+			return numbers[i]
 		}
 	}
+
+	return -1
+}
+
+func findSetWithSum(number int, values []int) []int {
+	for i, _ := range values {
+		sum := values[i]
+
+		if sum >= number {
+			continue
+		}
+
+		for j := i + 1; j < len(values); j++ {
+			sum += values[j]
+
+			if sum > number {
+				break
+			}
+
+			if sum == number {
+				return values[i : j+1]
+			}
+		}
+	}
+
+	return []int{}
 }
 
 func sumExists(number int, values []int) bool {
@@ -26,7 +59,7 @@ func sumExists(number int, values []int) bool {
 				continue
 			}
 
-			if val1 + val2 != number {
+			if val1+val2 != number {
 				continue
 			}
 
